db97b1e13ba193cdf17b23dc86e07cf49e8a15ad
Xuechun Lu <xuechunlu7@gmail.com>
2020-11-29 21:07:03 -0500

    ANN added

--- model/ANN.R
+++ model/ANN.R
@@ -10,10 +10,10 @@ head(X_PCA)
 
 
 
-library(neuralnet)
-model<-neuralnet(Response~.,data = X_CFS, hidden = 3)
-hist(predict(model,X_CFS[,-35]))
-X_CFS$Response<-
+# library(neuralnet)
+# model<-neuralnet(Response~.,data = X_CFS, hidden = 3)
+# hist(predict(model,X_CFS[,-35]))
+# X_CFS$Response<-
 
 library(nnet)
 X_CFS$Response<-as.factor(X_CFS$Response)
@@ -22,9 +22,28 @@ nn1 <- nnet(Response~., data = X_CFS, size = 20, rang = 0.1,
 
 pred<-predict(nn1, X_CFS[,-35],type="class") 
 pred
-
+# error rate
+sum(pred!=y)/69281
 # RMSE
 sum((as.numeric(pred)-as.numeric(y))^2)/69281
 # MAE
 sum(abs(as.numeric(pred)-as.numeric(y)))/69281
+
+
+
+
+
+# PCA
+X_PCA_2<-cbind(X_PCA,y)
+head(X_PCA_2)
+nn2 <- nnet(y~., data = X_PCA_2, size = 20, rang = 0.1,
+            decay = 5e-4, maxit = 200, na.action = na.omit)
+pred2 <- predict(nn2, X_PCA,type="class") 
+pred2
+
+sum(pred2!=y)/69281
+# RMSE
+sum((as.numeric(pred2)-as.numeric(y))^2)/69281
+# MAE
+sum(abs(as.numeric(pred2)-as.numeric(y)))/69281
 
